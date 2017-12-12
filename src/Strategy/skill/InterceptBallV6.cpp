cf8f986948e7f98fdd1c9352abd65c612b1184f7
Wayne's Laptop <wenlc@zju.edu.cn>
2017-12-12 19:58:28 +0700

    recovery to stable version

--- src/Strategy/skill/InterceptBallV6.cpp
+++ src/Strategy/skill/InterceptBallV6.cpp
@@ -9,11 +9,11 @@
 std::string CParamReader::_paramFileName = "zeus2005";
 namespace {
 	int MAX_CNT_FOR_TURN = 6;
-	double RESPONSE_TIME = 0.3;
+	double RESPONSE_TIME = 0.4;
 	double FRICTION;
 }
 
-CInterceptBallV6::CInterceptBallV6()
+CInterceptBallV6::CInterceptBallV6():waitPoint(0, 0)
 {
 	DECLARE_PARAM_READER_BEGIN(FieldParam)
 	READ_PARAM(FRICTION)
@@ -27,20 +27,13 @@ CInterceptBallV6::~CInterceptBallV6()
 
 void CInterceptBallV6::plan(const CVisionModule * pVision)
 {
-	double FRICTION;
-
 	const MobileVisionT& ball = pVision->Ball();
 	const int robotNum = task().executor;
-	//const double power = 500;//task().player.kickpower;
 	const double finalAngel = task().player.angle;
 	const PlayerVisionT& me = pVision->OurPlayer(robotNum);
 	const int playerFlag = task().player.flag;
 
 
-	//CGeoSegment ballLine(CGeoPoint(ball.RawPos().x() - 9999 * ball.VelX(), ball.RawPos().y() - 9999 * ball.VelY()),
-	//	CGeoPoint(ball.RawPos().x() + 9999 * ball.VelX(), ball.RawPos().y() + 9999 * ball.VelY()));
-	//CGeoSegment me2Goal(me.RawPos(), CGeoPoint(450, 0));
-	//bool onDiffSide = ballLine.IsSegmentsIntersect(me2Goal);//判断车、门是否在异侧
 	CGeoLine ballLine(ball.RawPos(), ball.Vel().dir());
 	TaskT chase_kick_task(task());
 	double ballArriveTime = 0;
@@ -48,10 +41,10 @@ void CInterceptBallV6::plan(const CVisionModule * pVision)
 	int testMinFrame = 5;//最少帧数
 	CGeoPoint testPoint(ball.RawPos().x(), ball.RawPos().y());
 	CVector me2Ball = ball.RawPos() - me.RawPos();
-	//CVector ball2Goal = CGeoPoint(450, 0) - ball.RawPos();
-	//CVector me2Goal = CGeoPoint(450, 0) - me.RawPos();
 	CGeoPoint ballLineProjection = ballLine.projection(me.RawPos());
 	CVector ball2Projection = ballLineProjection - ball.RawPos();
+	CVector projection2Me = me.RawPos() - ballLineProjection;
+
 
 	double isSensored = RobotSensor::Instance()->IsInfoValid(robotNum) && RobotSensor::Instance()->IsInfraredOn(robotNum);	//是否有检测到红外
 	double maxArriveTime = 5;//车最多移动时间
@@ -75,8 +68,8 @@ void CInterceptBallV6::plan(const CVisionModule * pVision)
 		if (abs(me.Dir() - finalAngel) < 3*Param::Math::PI / 180) {			//need shoot  ball2Goal.dir())
 			cnt = 0;
 			needDribble = false;
+			//For kick status
 			//KickStatus::Instance()->setKick(robotNum, power);
-			return;
 		}
 		else {												//need turn off
 			int  CLOCKWISE = -1;	//-1 顺时针
@@ -86,69 +79,68 @@ void CInterceptBallV6::plan(const CVisionModule * pVision)
 			chase_kick_task.player.pos = CGeoPoint(ball.RawPos().x() - me2Ball.mod()*std::cos(newdir), ball.RawPos().y() - me2Ball.mod()*std::sin(newdir));
 			double omega;
 			if (abs(me.Dir() - finalAngel) < Param::Math::PI / 6) {
-				omega = Param::Math::PI/2;
-				chase_kick_task.player.speed_x = 20;
+				omega = Param::Math::PI / 3 * 2;
+				chase_kick_task.player.speed_x = 30;
 			}
 			else {
-				omega = Param::Math::PI;
+				omega = Param::Math::PI*1.5;
 				chase_kick_task.player.speed_x = 40;
 			}
 			chase_kick_task.player.speed_y = -CLOCKWISE*omega*me2Ball.mod();
 			chase_kick_task.player.rotate_speed = CLOCKWISE*omega;
-			//std::cout << "open speed!!" << std::endl;
 			setSubTask(TaskFactoryV2::Instance()->OpenSpeed(chase_kick_task));
-			return;
 		}
 	}
+	else if (abs(testVel) < 30) {				//球近似静止
+			if (abs(Utils::Normalize(me2Ball.dir() - me.Dir())) > Param::Math::PI / 3 * 2 && me2Ball.mod() < 30) {
+				chase_kick_task.player.pos = ball.RawPos() + Utils::Polar2Vector(30, me.Dir() + Param::Math::PI / 2);
+			}
+			else {
+				chase_kick_task.player.pos = ball.RawPos();
+			}
+			chase_kick_task.player.angle = me2Ball.dir();
+			setSubTask(TaskFactoryV2::Instance()->GotoPosition(chase_kick_task));
+		}
+		else if (me.RawPos().dist(ballLineProjection) < 23 && me2Ball.mod()<100 &&
+		abs(Utils::Normalize(ball2Projection.dir()) - Utils::Normalize(ball.Vel().dir()))<0.1) {
+			if (abs(Utils::Normalize(me2Ball.dir() - ball.Vel().dir())) > Param::Math::PI / 3 * 2)
+			{
+				chase_kick_task.player.pos = ballLineProjection;
+			}
+			else
+			{
+				chase_kick_task.player.pos = ballLineProjection + ball.Vel() * 2;
+			}
+			chase_kick_task.player.angle = lastAngle;
+			setSubTask(TaskFactoryV2::Instance()->GotoPosition(chase_kick_task));
+		}
+		else {							//计算
+			do {
+				ballArriveTime += testMinFrame * 1.0 / 60.0;
+				testVel = ball.Vel().mod() - ballAcc*ballArriveTime;
+				testBallLength = (ball.Vel().mod() + testVel)*ballArriveTime / 2;//(ball.Vel().mod2() - testVel*testVel) / (2 * ballAcc);
+				testPoint.setX(ball.RawPos().x() + testBallLength * std::cos(ball.Vel().dir()));
+				testPoint.setY(ball.RawPos().y() + testBallLength * std::sin(ball.Vel().dir()));
+				meArriveTime = predictedTime(me, testPoint);
+			} while (IsInField(testPoint) && (meArriveTime + RESPONSE_TIME) > ballArriveTime && meArriveTime < maxArriveTime);
 
-	if (abs(testVel) < 30) {				//球近似静止
-		chase_kick_task.player.pos = ball.RawPos();
-		chase_kick_task.player.angle = me2Ball.dir();
-		setSubTask(TaskFactoryV2::Instance()->GotoPosition(chase_kick_task));
-		return;
-	}
-	if (me.RawPos().dist(ballLineProjection) < 10 && me2Ball.mod()<50
-		&& abs(Utils::Normalize(ball2Projection.dir()) - Utils::Normalize(ball.Vel().dir()))<0.1) {
-		//std::cout << "in wait :" << me.RawPos().dist(ballLineProjection) << std::endl;
-		chase_kick_task.player.pos = ballLineProjection;
-		chase_kick_task.player.angle = me2Ball.dir();
-		setSubTask(TaskFactoryV2::Instance()->GotoPosition(chase_kick_task));
-		return;
-	}
 
-	/*if (me.RawPos().dist(ballLineProjection) < 20 //&& me2Ball.mod() < 40
-	&& 	abs(Utils::Normalize(ball2Projection.dir()) - Utils::Normalize(ball.Vel().dir())) - Param::Math::PI < 0.2) {
-	std::cout << "in chase:" << me.RawPos().dist(ballLineProjection) << std::endl;
-	/*chase_kick_task.player.pos = CGeoPoint(ball.RawPos().x() + ball.VelX() / 2, ball.RawPos().y() + ball.VelY() / 2);//ballLineProjection;
-	chase_kick_task.player.angle = me2Ball.dir();
-	setSubTask(TaskFactoryV2::Instance()->GotoPosition(chase_kick_task));
+			CVector testpoint2Ball = ball.RawPos() - testPoint;//CGeoPoint(450, 0)- testPoint;
+			chase_kick_task.player.angle = testpoint2Ball.dir();
+			lastAngle = testpoint2Ball.dir();
 
-	chase_kick_task.player.speed_x = 2;
-	setSubTask(TaskFactoryV2::Instance()->OpenSpeed(chase_kick_task));
+			if (abs(Utils::Normalize(me2Ball.dir() - ball.Vel().dir())) < Param::Math::PI / 2 && me2Ball.mod() <= 40)
+			{
+				chase_kick_task.player.pos = testPoint + (projection2Me / projection2Me.mod() * 30);
+			}
+			else
+			{
+				chase_kick_task.player.pos = testPoint;
+			}
+			setSubTask(TaskFactoryV2::Instance()->SmartGotoPosition(chase_kick_task));
+		}
+	CStatedTask::plan(pVision);
 	return;
-	}*/
-
-
-	do {
-		ballArriveTime += testMinFrame * 1.0 / 60.0;
-		testVel = ball.Vel().mod() - ballAcc*ballArriveTime;
-		testBallLength = (ball.Vel().mod() + testVel)*ballArriveTime / 2;//(ball.Vel().mod2() - testVel*testVel) / (2 * ballAcc);
-		testPoint.setX(ball.RawPos().x() + testBallLength * std::cos(ball.Vel().dir()));
-		testPoint.setY(ball.RawPos().y() + testBallLength * std::sin(ball.Vel().dir()));
-		meArriveTime = predictedTime(me, testPoint);
-		//std::cout << ballAcc<<"ball Arrive@" << ballArriveTime << "\tme Arrive @" << meArriveTime << std::endl;
-	} while (IsInField(testPoint) && (meArriveTime + RESPONSE_TIME) > ballArriveTime && meArriveTime < maxArriveTime);
-
-
-	CVector testpoint2Ball = ball.RawPos() - testPoint;//CGeoPoint(450, 0)- testPoint;
-	if (me2Ball.mod()>100 ) 
-	chase_kick_task.player.angle = testpoint2Ball.dir();
-	else 
-	chase_kick_task.player.angle = me2Ball.dir();
-	chase_kick_task.player.pos = testPoint;//CGeoPoint(testPoint.x() - 8 * testpoint2Goal.x() / testpoint2Goal.mod(), testPoint.y() - 8 * testpoint2Goal.y() / testpoint2Goal.mod());
-
-	setSubTask(TaskFactoryV2::Instance()->GotoPosition(chase_kick_task));
-
 }
 
 
